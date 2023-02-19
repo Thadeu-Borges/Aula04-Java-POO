53bff954b200884eb3db5921446fccfb25850b5a
Thadeu <123416610+Thadeu-Borges@users.noreply.github.com>
2023-02-19 04:49:48 -0300

    Java POO

---
+++ src/aula04/Aula04.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula04;
+
+/**
+ *
+ * @author AAAA
+ */
+public class Aula04 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Caneta c1 = new Caneta("NIC", "Amarela", 0.4f);
+        c1.status();
+
+        Caneta c2 = new Caneta("KKK", "Laranja", 1.5f);
+        c2.status();
+    }
+
+}
